ffa03736755d3a18c83dfae2e8330a02a125b473
Dustin Dettmer <dustinpaystaxes@gmail.com>
2021-08-04 15:29:26 -0700

    Add &GCCFLAGS option to compile script

--- fast_compile.sh
+++ fast_compile.sh
@@ -5,15 +5,15 @@ cd code
 for file in *.c
 do
  filename=$(echo "$file" | tr / _)
- echo gcc -g -c "$file" -o objects/"${filename%.*}".o
- gcc -g -c "$file" -o objects/"${filename%.*}".o
+ echo gcc $GCCFLAGS -g -c "$file" -o objects/"${filename%.*}".o
+ gcc $GCCFLAGS -g -c "$file" -o objects/"${filename%.*}".o
 done
 
 for file in *.cpp
 do
  filename=$(echo "$file" | tr / _)
- echo ++ -g -c "$file" -o objects/"${filename%.*}".o
- g++ -g -c "$file" -o objects/"${filename%.*}".o
+ echo g++ $GCCFLAGS -g -c "$file" -o objects/"${filename%.*}".o
+ g++ $GCCFLAGS -g -c "$file" -o objects/"${filename%.*}".o
 done
 
 ar rvs objects/all.a objects/*.o
@@ -21,7 +21,7 @@ ar rvs objects/all.a objects/*.o
 cd ..
 
 echo "Compiling test"
-gcc -g test/test.c code/objects/all.a libraries/objects/all.a -lgmp -lstdc++ -o test/test
+gcc $GCCFLAGS -g test/test.c code/objects/all.a libraries/objects/all.a -lgmp -lstdc++ -o test/test
 
 echo "Compiling example"
-gcc -g -Icode test/example.c code/objects/all.a libraries/objects/all.a -lsqlite3 -lgmp -lstdc++ -o test/example
+gcc $GCCFLAGS -g -Icode test/example.c code/objects/all.a libraries/objects/all.a -lsqlite3 -lgmp -lstdc++ -o test/example
