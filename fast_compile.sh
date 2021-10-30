3c704b8672702ba34d3e641a0ccc9d79ad028315
Dustin Dettmer <dustinpaystaxes@gmail.com>
2021-10-29 23:50:24 -0700

    Add webserver_example to fast_compile as well as GCC/GPP options

--- fast_compile.sh
+++ fast_compile.sh
@@ -2,18 +2,21 @@
 
 cd code
 
+GCC=gcc
+GPP=g++
+
 for file in *.c
 do
  filename=$(echo "$file" | tr / _)
- echo gcc $GCCFLAGS -g -c "$file" -o objects/"${filename%.*}".o
- gcc $GCCFLAGS -g -c "$file" -o objects/"${filename%.*}".o
+ echo $GCC $GCCFLAGS -g -c "$file" -o objects/"${filename%.*}".o
+ $GCC $GCCFLAGS -g -c "$file" -o objects/"${filename%.*}".o
 done
 
 for file in *.cpp
 do
  filename=$(echo "$file" | tr / _)
- echo g++ $GCCFLAGS -g -c "$file" -o objects/"${filename%.*}".o
- g++ $GCCFLAGS -g -c "$file" -o objects/"${filename%.*}".o
+ echo $GPP $GCCFLAGS -g -c "$file" -o objects/"${filename%.*}".o
+ $GPP $GCCFLAGS -g -c "$file" -o objects/"${filename%.*}".o
 done
 
 ar rvs objects/all.a objects/*.o
@@ -21,7 +24,10 @@ ar rvs objects/all.a objects/*.o
 cd ..
 
 echo "Compiling test"
-gcc $GCCFLAGS -g test/test.c code/objects/all.a libraries/objects/all.a -lgmp -lstdc++ -o test/test
+$GCC $GCCFLAGS -g test/test.c code/objects/all.a libraries/objects/all.a -lgmp -lstdc++ -o test/test
 
 echo "Compiling example"
-gcc $GCCFLAGS -g -Icode test/example.c code/objects/all.a libraries/objects/all.a -lsqlite3 -lgmp -lstdc++ -o test/example
+$GCC $GCCFLAGS -g -Icode test/example.c code/objects/all.a libraries/objects/all.a -lsqlite3 -lgmp -lstdc++ -o test/example
+
+echo "Compiling webserver_example"
+$GCC $GCCFLAGS -g -Icode test/webserver_example.c code/objects/all.a libraries/objects/all.a -lsqlite3 -lgmp -lstdc++ -o test/webserver_example
